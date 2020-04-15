8f003e29fd117ba27dc1a6b3bbda94cb218606d7
AdmiralGallade <mailforkushagra2@gmail.com>
2020-04-14 23:10:11 -0400

    Update Init.py

--- Init.py
+++ Init.py
@@ -1,2 +1,20 @@
-# Auto detect text files and perform LF normalization
-* text=auto
+import sys
+import pygame
+
+def run_game():
+    # Initialize game and create a screen object.
+     pygame.init()
+     screen = pygame.display.set_mode((1200, 800))
+     pygame.display.set_caption("Alien Invasion")
+     # Start the main loop for the game.
+     
+    # Watch for keyboard and mouse events.
+     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              sys.exit()
+
+    # Make the most recently drawn screen visible.
+     pygame.display.flip()
+
+run_game()
