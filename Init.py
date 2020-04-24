c7b7f3ce64b16cb161e997b2fbb6f154654ddfb3
AdmiralGallade <mailforkushagra2@gmail.com>
2020-04-24 00:22:34 -0400

    Added images

--- Init.py
+++ Init.py
@@ -1,20 +1,19 @@
 import sys
 import pygame
-
+from pygame import display
+from Settings import Settings
 def run_game():
-    # Initialize game and create a screen object.
-     pygame.init()
-     screen = pygame.display.set_mode((1200, 800))
-     pygame.display.set_caption("Alien Invasion")
-     # Start the main loop for the game.
-     
-    # Watch for keyboard and mouse events.
-     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-              sys.exit()
+ # Initialize pygame, settings, and screen object.
+ pygame.init()
+ ai_settings = Settings()
+ screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+ pygame.display.set_caption("Alien Invasion")
+ # Start the main loop for the game.
+ while True:
+ 
+ # Redraw the screen during each pass through the loop.
+  screen.fill(ai_settings.bg_color)
 
-    # Make the most recently drawn screen visible.
-     pygame.display.flip()
-
+ # Make the most recently drawn screen visible.
+ pygame.display.flip()
 run_game()
